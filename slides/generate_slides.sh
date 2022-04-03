267c5d3300c35e8d8671a119ea817dee1de093a6
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-04-03 16:36:32 -0400

    Switch to new slide template.

--- slides/generate_slides.sh
+++ slides/generate_slides.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
-# get paths and venv status
+# get paths
 SCRIPT=$(realpath "$0")
 SLIDES_DIR=$(dirname "$SCRIPT")
 REPO_DIR=$(dirname "$SLIDES_DIR")
-ACTIVE_ENV=$(basename $CONDA_DEFAULT_ENV)
 
-
-if [[ "$CONDA_DEFAULT_ENV" == "" ]]; then
-    echo "Virtual environment is not enabled. Quitting...";
+if [[ "$#" -ne 1 ]]; then
+    echo "Specify slide type as argument. Options:"
+    echo $(ls -d $SLIDES_DIR/templates/*/ | xargs -n 1 basename | awk -vORS=', ' '{ print $1; }' | sed 's/, $/\n/');
 else
-    if [[ "$CONDA_DEFAULT_ENV" != "data_viz_workshop" ]]; then
-        echo "The data_viz_workshop conda env is not activated.";
+    TEMPLATE_TYPE="$1";
+
+    # get venv status
+    ACTIVE_ENV=$(basename $CONDA_DEFAULT_ENV)
+
+    if [[ "$CONDA_DEFAULT_ENV" == "" ]]; then
+        echo "Virtual environment is not enabled. Quitting...";
     else
-        # if nbmerge isn't installed, do so
-        echo "Checking for nbmerge..."
-        pip3 freeze | grep nbmerge || pip3 install nbmerge;
+        if [[ "$CONDA_DEFAULT_ENV" != "data_viz_workshop" ]]; then
+            echo "The data_viz_workshop conda env is not activated.";
+        else
+            # if nbmerge isn't installed, do so
+            echo "Checking for nbmerge..."
+            pip3 freeze | grep nbmerge || pip3 install nbmerge;
 
-        # use nbmerge to combine all slide notebooks into a single notebook
-        echo "[nbmerge] Creating a combined notebook for all slides..."
-        COMBINED_NOTEBOOK="$SLIDES_DIR"/workshop.ipynb
-        nbmerge $SLIDES_DIR/*.ipynb -o $COMBINED_NOTEBOOK;
+            # use nbmerge to combine all slide notebooks into a single notebook
+            echo "[nbmerge] Creating a combined notebook for all slides..."
+            COMBINED_NOTEBOOK="$SLIDES_DIR/workshop.ipynb"
+            nbmerge $SLIDES_DIR/*.ipynb -o $COMBINED_NOTEBOOK;
 
-        # make all slide decks
-        jupyter nbconvert --to slides --output-dir "$SLIDES_DIR"/html "$SLIDES_DIR"/*.ipynb;
+            # make all slide decks
+            jupyter nbconvert \
+                --to slides \
+                --template=$TEMPLATE_TYPE \
+                --TemplateExporter.extra_template_basedirs="$SLIDES_DIR"/templates \
+                --output-dir "$SLIDES_DIR"/html \
+                "$SLIDES_DIR"/*.ipynb;
 
-        # delete the combined notebook
-        echo "Cleaning up..."
-        rm $COMBINED_NOTEBOOK
+            # delete the combined notebook
+            echo "Cleaning up..."
+            rm $COMBINED_NOTEBOOK
 
-        echo "Done!";
+            echo "Done!";
+        fi
     fi
+
 fi
