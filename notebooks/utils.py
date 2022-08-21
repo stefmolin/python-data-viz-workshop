4858e069b39c7050179cab56b7c22d3817ddbfe3
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2022-08-21 18:42:06 -0400

    Move config for the reproducible Matplotlib SVG output to utils; remove %matplotlib inline.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,6 +1,7 @@
 """Utility functions for the workshop."""
 
 from packaging.version import LegacyVersion as Version
+import datetime as dt
 import importlib
 import json
 import os
@@ -68,3 +69,14 @@ def despine(ax):
     for spine in ['top', 'right']:
         ax.spines[spine].set_visible(False)
     return ax
+
+def mpl_svg_config(hashsalt):
+    """Help configure the SVG backend for Matplotlib and make it reproducible."""
+    from matplotlib import rc
+    rc('svg', hashsalt=hashsalt)
+
+    return {
+        'metadata': {
+            'Date': f"(c) 2021-{dt.date.today().year} Stefanie Molin"
+        }
+    }
--- slides/utils.py
+++ slides/utils.py
@@ -1,7 +1,21 @@
 """Utility functions for the workshop."""
 
+import datetime as dt
+
+
 def despine(ax):
     """Hide the top and right spines on a Matplotlib Axes object."""
     for spine in ['top', 'right']:
         ax.spines[spine].set_visible(False)
     return ax
+
+def mpl_svg_config(hashsalt):
+    """Help configure the SVG backend for Matplotlib and make it reproducible."""
+    from matplotlib import rc
+    rc('svg', hashsalt=hashsalt)
+
+    return {
+        'metadata': {
+            'Date': f"(c) 2021-{dt.date.today().year} Stefanie Molin"
+        }
+    }
