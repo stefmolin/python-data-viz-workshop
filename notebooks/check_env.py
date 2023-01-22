b6d19ce0c0a2c2989682e96d0c516f25ac1fc235
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2023-01-22 16:24:24 -0500

    Clean up check_env.py script.

--- notebooks/check_env.py
+++ notebooks/check_env.py
@@ -1,5 +1,6 @@
 """Check the setup for the workshop."""
 
+from functools import partial
 from packaging.version import Version
 import importlib
 import json
@@ -8,25 +9,56 @@ import sys
 import yaml
 
 
+
+def _print_version_ok(item):
+    """
+    Print an OK message for version check.
+
+    Parameters
+    ----------
+    item : str
+        The item being inspected (package, tool, etc.).
+    """
+    print('\x1b[42m[ OK ]\x1b[0m', '%s' % item)
+
+def _print_version_failure(item, req_version, version, failures):
+    """
+    Print a failure message for version check.
+
+    Parameters
+    ----------
+    item : str
+        The item being inspected (package, tool, etc.).
+    req_version : str
+        The required version.
+    version : str
+        The version currently installed.
+    failures : list
+        The list of items currently failing the environment check.
+    """
+    failures.append(item)
+    if version:
+        msg = '%s version %s is required, but %s installed.'
+        values = (item, req_version, version)
+    else:
+        msg = '%s is not installed.'
+        values = item
+    print('\x1b[41m[FAIL]\x1b[0m', msg % values)
+
 def run_env_check(raise_exc=False):
-    """Check that the packages we need are installed and the Python version is correct."""
+    """
+    Check that the packages we need are installed and, if necessary,
+    whether the Python version is correct.
+
+    Parameters
+    ----------
+    raise_exc : bool, default ``False``
+        Whether to raise an `Exception` if any of the packages doesn't
+        match the requirements (used for GitHub Action).
+    """
     
     failures = []
-
-    def _print_version_ok(item):
-        """Print an OK message for version check."""
-        print('\x1b[42m[ OK ]\x1b[0m', '%s' % item)
-
-    def _print_version_failure(item, req_version, version):
-        """Print a failure message for version check."""
-        failures.append(item)
-        if version:
-            msg = '%s version %s is required, but %s installed.'
-            values = (item, req_version, version)
-        else:
-            msg = '%s is not installed.'
-            values = item
-        print('\x1b[41m[FAIL]\x1b[0m', msg % values)
+    _print_failure = partial(_print_version_failure, failures=failures)
 
     # read in the environment file and process versions
     with open('../environment.yml', 'r') as file:
@@ -53,10 +85,12 @@ def run_env_check(raise_exc=False):
     try:
         required_version = requirements.pop('python')
         python_version = sys.version_info
-        for component, value in zip(['major', 'minor', 'micro'], required_version.split('.')):
+        for component, value in zip(
+            ['major', 'minor', 'micro'], required_version.split('.')
+        ):
             if getattr(python_version, component) != int(value):
                 print(f'Using Python at {sys.prefix}:\n-> {sys.version}')
-                _print_version_failure(
+                _print_failure(
                     'Python',
                     required_version,
                     f'{python_version.major}.{python_version.minor}'
@@ -77,14 +111,19 @@ def run_env_check(raise_exc=False):
                     min_version, max_version = req_version
                     if (
                         installed_version < Version(min_version).base_version
-                        or (max_version and installed_version > Version(max_version).base_version)
+                        or (
+                            max_version and
+                            installed_version > Version(max_version).base_version
+                        )
                     ):
-                        _print_version_failure(
-                            pkg, f'>= {min_version}{f" and <= {max_version}" if max_version else ""}', version
+                        _print_failure(
+                            pkg,
+                            f'>= {min_version}{f" and <= {max_version}" if max_version else ""}',
+                            version
                         )
                         continue
                 elif Version(version).base_version != Version(req_version).base_version:
-                    _print_version_failure(pkg, req_version, version)
+                    _print_failure(pkg, req_version, version)
                     continue
             _print_version_ok(pkg)
         except ImportError:
@@ -100,23 +139,26 @@ def run_env_check(raise_exc=False):
                                     installed_version < Version(min_version)
                                     or (max_version and installed_version > Version(max_version))
                                 ):
-                                    _print_version_failure(
+                                    _print_failure(
                                         pkg,
                                         f'>= {min_version}{f" and <= {max_version}" if max_version else ""}',
                                         pkg_info['version']
                                     )
                                     continue
                             elif pkg_info['version'] != req_version:
-                                _print_version_failure(pkg, req_version, pkg_info['version'])
+                                _print_failure(pkg, req_version, pkg_info['version'])
                                 continue
                         _print_version_ok(pkg)
                         continue
                 except IndexError:
                     pass
-            _print_version_failure(pkg, req_version, None)
+            _print_failure(pkg, req_version, None)
 
     if failures and raise_exc:
-        raise Exception('Environment failed inspection.')
+        raise Exception(
+            'Environment failed inspection due to incorrect versions '
+            f'of {len(failures)} item(s): {", ".join(failures)}.'
+        )
 
 if __name__ == '__main__':
     print(f'Using Python at {sys.prefix}:\n-> {sys.version}')
